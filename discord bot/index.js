9122ccdf9546978bd5a33062df49358d88356db9
Andrew Harper <115321970+KoopaCode@users.noreply.github.com>
2023-08-15 05:15:24 -0400

    Main Upload

---
+++ discord bot/index.js
@@ -0,0 +1,237 @@
+﻿const Discord = require('discord.js');
+const mysql = require('mysql');
+
+const mysqlConfig = {
+  host: 'your_host_name',
+  user: 'user_username',
+  password: 'your_password',
+  database: 'your_database_name'
+};
+
+const botToken = 'Bot_Token';
+
+
+
+const pool = mysql.createPool(mysqlConfig);
+
+
+const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
+
+client.once('ready', () => {
+  console.log('Bot is ready!');
+  createSlashCommands();
+  client.user.setActivity('Koopa Auth v2', { type: 'WATCHING' });
+});
+
+
+async function createSlashCommands() {
+  try {
+    const commands = [
+      {
+        name: 'reset',
+        description: 'Reset HWID',
+        options: [
+          {
+            name: 'key',
+            description: 'The key to reset',
+            type: 'STRING',
+            required: true
+          }
+        ]
+      },
+      {
+        name: 'create',
+        description: 'Create a new key',
+        options: [
+          {
+            name: 'type',
+            description: 'Expiration time of the key',
+            type: 'STRING',
+            required: true,
+            choices: [
+              {
+                name: 'Week',
+                value: '1'
+              },
+              {
+                name: 'Month',
+                value: '2'
+              },
+              {
+                name: 'Lifetime',
+                value: '3'
+              }
+            ]
+          },
+          {
+            name: 'key',
+            description: 'The key to create',
+            type: 'STRING',
+            required: false
+          }
+        ]
+      },
+      {
+        name: 'check',
+        description: 'Check key information',
+        options: [
+          {
+            name: 'key',
+            description: 'The key to check',
+            type: 'STRING',
+            required: true
+          }
+        ]
+      },
+    ];
+
+    const guildId = 'Your Guild ID'; // Put Your Guild ID Here
+
+    await client.guilds.cache.get(guildId)?.commands.set(commands);
+    console.log('Slash commands registered.');
+  } catch (error) {
+    console.error('Error registering slash commands:', error);
+  }
+}
+
+
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName, options } = interaction;
+
+  if (commandName === 'reset' && isAuthorizedUser(interaction.user.id)) {
+    const key = options.getString('key');
+
+    try {
+      const connection = await getConnection();
+      const query = 'UPDATE auth SET hwid = NULL WHERE `key` = ?';
+      await executeQuery(connection, query, [key]);
+      connection.release();
+
+      interaction.reply('HWID reset successfully.');
+    } catch (err) {
+      console.error('Error:', err);
+      interaction.reply('An error occurred.');
+    }
+  } else if (commandName === 'create' && isAuthorizedUser(interaction.user.id)) {
+    let key = options.getString('key');
+    const type = options.getString('type');
+    if (!key) {
+
+      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+      key = Array.from({ length: 25 }, () => characters[Math.floor(Math.random() * characters.length)]).join('');
+      key = key.replace(/(.{5})/g, '$1-').slice(0, -1);
+    }
+
+    try {
+      const connection = await getConnection();
+      const query = 'INSERT INTO auth (`key`, `type`) VALUES (?, ?)';
+      await executeQuery(connection, query, [key, type]);
+      connection.release();
+
+      const embed = new Discord.MessageEmbed()
+        .setTitle('Key Created Successfully')
+        .addField('Key', '```xl\n' + key + '\n```')
+        .addField('Type', type);
+      const button = new Discord.MessageButton()
+        .setLabel('Download')
+        .setStyle('LINK')
+        .setURL('https://koopa.lol'); // replace koopa.lol with your download url
+
+      interaction.reply({ embeds: [embed], components: [new Discord.MessageActionRow().addComponents(button)] });
+    } catch (err) {
+      console.error('Error:', err);
+      interaction.reply('An error occurred.');
+    }
+  } else if (commandName === 'check' && isAuthorizedUser(interaction.user.id)) {
+    const key = options.getString('key');
+
+    try {
+      const connection = await getConnection();
+      const query = 'SELECT * FROM auth WHERE `key` = ?';
+      const result = await executeQuery(connection, query, [key]);
+      connection.release();
+
+      if (result.length > 0) {
+        const authData = result[0];
+        const embed = new Discord.MessageEmbed()
+          .setTitle('Key Information')
+          .addField('Key', '```xl\n' + authData.key + '\n```')
+          .addField('Type', authData.type)
+          .addField('HWID', authData.hwid || 'Not set');
+
+        interaction.reply({ embeds: [embed] });
+      } else {
+        interaction.reply('Key not found.');
+      }
+    } catch (err) {
+      console.error('Error:', err);
+      interaction.reply('An error occurred.');
+    }
+  } else if (commandName === 'claim' && isAuthorizedUser(interaction.user.id)) {
+    const key = options.getString('key');
+
+    try {
+      const connection = await getConnection();
+      const query = 'SELECT * FROM auth WHERE `key` = ?';
+      const result = await executeQuery(connection, query, [key]);
+      connection.release();
+
+      if (result.length > 0) {
+        const authData = result[0];
+        const guildId = interaction.guild.id;
+        const memberId = interaction.member.id;
+        const guild = client.guilds.cache.get(guildId);
+        const member = guild.members.cache.get(memberId);
+        
+        if (member.roles.cache.has(claimRoleId)) {
+          interaction.reply('You have already claimed the role.');
+        } else {
+          member.roles.add(claimRoleId);
+          interaction.reply('Role claimed successfully.');
+        }
+      } else {
+        interaction.reply('Invalid key.');
+      }
+    } catch (err) {
+      console.error('Error:', err);
+      interaction.reply('An error occurred.');
+    }
+  } 
+});
+
+
+function isAuthorizedUser(userId) {
+  return userId === 'User ID' || userId === 'User ID';
+}
+
+
+function getConnection() {
+  return new Promise((resolve, reject) => {
+    pool.getConnection((err, connection) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(connection);
+    });
+  });
+}
+
+
+function executeQuery(connection, query, params) {
+  return new Promise((resolve, reject) => {
+    connection.query(query, params, (err, result) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(result);
+    });
+  });
+}
+
+
+client.login(botToken);
---
+++ main script/main.py
@@ -0,0 +1,179 @@
+import sys
+import requests
+import os
+import time
+from datetime import datetime, timedelta
+import wmi
+import mysql.connector
+import json
+
+MYSQL_CONFIG = {
+    'user': 'user_username',
+    'password': 'your_password',
+    'host': 'your_host_name',
+    'database': 'your_database_name'
+}
+
+class Theme:
+    MAGENTA = '\033[38;5;213m'
+    RESET = '\033[0m'
+
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def get_hwid():
+    c = wmi.WMI()
+    disk = c.Win32_DiskDrive()[0]
+    return disk.SerialNumber
+
+def set_expiration_time(cursor, key_type):
+    current_time = get_current_time()
+
+    if key_type == 1:
+        expiration_time = current_time + timedelta(weeks=1)
+    elif key_type == 2:
+        expiration_time = current_time + timedelta(days=30)
+    elif key_type == 3:
+        expiration_time = current_time + timedelta(days=3650)
+
+    return expiration_time
+
+def check_key(key):
+    try:
+        cnx = mysql.connector.connect(**MYSQL_CONFIG)
+        cursor = cnx.cursor()
+
+        query = "SELECT `hwid`, `type`, `expiration_time` FROM auth WHERE `key` = %s"
+        cursor.execute(query, (key,))
+        result = cursor.fetchone()
+
+        if result:
+            stored_hwid = result[0]
+            key_type = result[1]
+            expiration_time = result[2]
+
+            if stored_hwid:
+                current_hwid = get_hwid()
+
+                if current_hwid != stored_hwid:
+                    print(f"Invalid HWID.")
+                    time.sleep(1.5)
+                    return
+
+            else:
+                current_hwid = get_hwid()
+
+                query = "UPDATE auth SET `hwid` = %s WHERE `key` = %s"
+                cursor.execute(query, (current_hwid, key))
+                cnx.commit()
+
+                print(f"HWID set.")
+                time.sleep(1.5)
+                clear_terminal()
+
+            current_time = get_current_time()
+            if expiration_time and current_time > expiration_time:
+                print(f"Key has expired.")
+                time.sleep(1.5)
+                update_key("")  
+                return
+
+            expiration_time = set_expiration_time(cursor, key_type)
+            cnx.commit()
+
+            update_config(key)  
+            send_discord_webhook(
+                key, get_hwid(), get_ip(), expiration_time
+            )
+
+        else:
+            print(f"Invalid key.")
+            time.sleep(1.5)
+            update_key("") 
+
+        cursor.close()
+        cnx.close()
+    except mysql.connector.Error as err:
+        print("Error:", err)
+
+def get_current_time():
+    return datetime.now()
+
+def get_ip():
+    response = requests.get('https://api.ipify.org?format=json')
+    data = response.json()
+    return data['ip']
+
+def send_discord_webhook(key, hwid, ip, expires):
+    webhook_url = 'WebHook URL' # Your Webhook URL
+
+    embed = {
+        'title': 'Login Information',
+        'color': 0x00ff00,
+        'fields': [
+            {'name': 'Key', 'value': f'```{key}```'},
+            {'name': 'HWID', 'value': f'```{hwid}```'},
+            {'name': 'IP', 'value': f'```{ip}```'},
+            {'name': 'Expires', 'value': f'```{expires}```'}
+        ]
+    }
+
+    data = {
+        'username': 'WebHook Name',
+        'embeds': [embed]
+    }
+
+    response = requests.post(webhook_url, json=data)
+    if response.status_code != 204:
+        print('Failed to send Discord webhook')
+
+def update_config(key):
+    config = read_config()
+    config['key'] = key
+    write_config(config)
+
+def update_key(key):
+    config = read_config()
+    config['key'] = key
+    write_config(config)
+
+def read_config():
+    try:
+        with open("config.json", "r") as config_file:
+            return json.load(config_file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {}
+
+def write_config(config):
+    with open("config.json", "w") as config_file:
+        json.dump(config, config_file, indent=4)
+
+def main_menu():
+    os.system("cls")
+    cmd = 'mode 120,30'
+    os.system(cmd)
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    config = read_config()
+    if 'key' not in config or not config['key']:
+        print("1 - Login\n2 - Exit")
+        option = input("Enter option: ")
+        if option == "1":
+            os.system("cls")
+            cmd = 'mode 62,3'
+            os.system(cmd)
+            key = input("Enter your license: ")
+            check_key(key)
+        elif option == "2":
+            sys.exit()
+        else:
+            main_menu()
+    else:
+        key = config['key']
+        check_key(key)
+
+if __name__ == "__main__":
+    main_menu()
+    os.system("cls")
+    cmd = 'mode 120,30'
+    os.system(cmd)
